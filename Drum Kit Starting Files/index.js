55d2d423713d658ebe1c5e65ee28dd653148b3e2
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2022-12-12 23:15:41 +0530

    Angela Yu Drum

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+
+for(var i=0;i<8;i++){
+document.querySelectorAll("button")[i].addEventListener("click", function(){
+    // var audio = new Audio('sounds/crash.mp3');
+    // audio.play();
+    this.style.color = "red";
+    });    
+}
